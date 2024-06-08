870ad13d5204b95bfb10b65fae6c6c2e31b23ff7
Tyler Bochard <zeruth09@gmail.com>
2024-06-08 00:32:27 -0400

    Initial Release

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("jvm") version "2.0.0"
+    `maven-publish`
+}
+
+group = "nulled"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/zeruth/annotations")
+            credentials {
+                username = System.getenv("USERNAME")
+                password = System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
+}
